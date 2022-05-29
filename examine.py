1b534d9760ee72b104e21d8d1023c9ade7082071
rmau2016 <105942987+rmau2016@users.noreply.github.com>
2022-05-29 10:30:17 -0400

    Update examine.py

--- examine.py
+++ examine.py
@@ -3,17 +3,13 @@ import os
 import io
 from io import StringIO
 
-r_file = io.open('file.txt', mode = 'r', buffering=-1, encoding=None, errors=None, closefd=True)
-w_file = io.open('md5.txt', 'a')
+r_file = io.open('textfile.txt', mode = 'r', buffering=-1, encoding=None, errors=None, closefd=True)
+w_file = io.open('md5.txt', 'w')
 m_1=[]
 for line in r_file:
-	string = r_file.readline()
-	file_like_io = StringIO(string)
 
-	for line in file_like_io:
-		m_1.append(line)
-		shortword = re.compile(r'\W*\b\w{1,31}\b')
-		print(shortword.sub('', line))
-		w_file.writelines(shortword.sub('',line))
+	match = re.findall(r'[\w.+-]+@[\w-]+\.[\w.-]+', line)
+	shortword = re.compile(r'\W*\b\w{1,31}\b')
+	w_file.writelines(str(shortword.sub("",line)).rstrip()+str(match)+"\n")
 		
 
